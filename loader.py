2c48081508d5d7040dd595bf44ab02a87b1c8af9
Dench991228 <denghanzhi85139@163.com>
2023-01-10 08:16:19 +0800

    初步完成了loader和训练部分

---
+++ loader.py
@@ -0,0 +1,31 @@
+import PIL
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import torch.utils.data
+
+
+normalize = transforms.Normalize(0.5, 0.5)
+img_size = 224
+batch_size = 32
+
+
+def get_train_loader(train_dir):
+    dataset = datasets.ImageFolder(train_dir, transforms.Compose([
+        transforms.RandomResizedCrop(img_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize
+    ]))
+    loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)
+    return loader
+
+
+def get_val_loader(val_dir):
+    dataset = datasets.ImageFolder(val_dir, transforms.Compose([
+        transforms.Resize(img_size, interpolation=PIL.Image.BICUBIC),
+        transforms.CenterCrop(img_size),
+        transforms.ToTensor(),
+        normalize
+    ]))
+    loader = torch.utils.data.DataLoader(dataset, batch_size=32, num_workers=4, shuffle=False)
+    return loader
---
+++ train.py
@@ -0,0 +1,55 @@
+import argparse
+
+import torch.optim
+import torch.nn.functional as f
+
+from loader import get_train_loader, get_val_loader
+from tqdm import tqdm
+from vit.vit import VisionTransformer
+
+
+arg_parser = argparse.ArgumentParser(description="My Implementation of Vision Transformer")
+# Basic arguments for training
+arg_parser.add_argument('data', metavar='DIR', help="Place your training data and validation data together in one folder")
+arg_parser.add_argument('epochs', type=int, default=90, help="Epochs for training")
+# Basic arguments for optimization
+arg_parser.add_argument('lr', type=float, default=0.1, help="Learning rate at start")
+arg_parser.add_argument('momentum', type=float, default=0.9)
+weight_decay = 1e-4
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """每隔30个Epoch就减小10倍学习率"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def train_one_epoch(model, optimizer, criterion, train_loader):
+    model.train()
+    avg_loss = 0
+    for idx, (images, targets) in tqdm(enumerate(train_loader)):
+        outputs = model(images)
+        outputs = f.softmax(outputs, dim=1)
+        loss = criterion(outputs, targets)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        avg_loss += loss
+    avg_loss /= len(train_loader)
+    print(f"Train Loss: {avg_loss}")
+
+
+
+if __name__ == '__main__':
+    args = arg_parser.parse_args()
+    count_epochs = args.epochs  # 训练多少个Epoch
+    data_dir = args.data  # 数据位置
+    learning_rate = args.lr  # 学习率
+    momentum = args.momentum  # 动量相关的设置
+    model = VisionTransformer(cls_head=True)  # 召唤一个基础的Vision Transformer，但是要有分类头
+    optimizer = torch.optim.SGD(model.parameters(), learning_rate, momentum, weight_decay=weight_decay)
+    criterion = torch.nn.CrossEntropyLoss()
+    for epoch in range(count_epochs):
+        print(f"Epoch number {epoch}")
+        train_one_epoch(model, optimizer, criterion, train_loader=get_train_loader(data_dir+"./train"))
